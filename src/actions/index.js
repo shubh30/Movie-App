b3dab6ebb5aac6a71ec663ee37b12ea9c76bc348
Shubham <shubhamnagpal30@gmail.com>
2020-07-06 15:12:59 +0530

    Add action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,13 @@
 // {
 //   type: 'DELETE-MOVIES'
 // }
+
+// Action types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 class App extends React.Component {
 
@@ -13,10 +14,7 @@ class App extends React.Component {
     });
     // Make api call
     // Dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log('State', this.props.store.getState()); 
   }
